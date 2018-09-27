f7bd547c30975ff9f15209d6138def0b2b12f9f8
Ahmed Wagdy <ahmed.w.amin@gmail.com>
2018-09-27 16:25:50 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+    # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404

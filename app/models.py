42d90d54b5eb05c451311a3f90c24cf538e3698a
lito <ahmed.w.amin@gmail.com>
2018-11-02 16:15:07 +0200

    changed all fields in spllier and clients

--- app/models.py
+++ app/models.py
@@ -69,8 +69,8 @@ class Supplier(models.Model):
 
 class Client(models.Model):
     client_name = models.CharField(max_length=85)
-    client_mobile_number = models.IntegerField()
-    client_ID_number = models.IntegerField()
+    client_mobile_number = models.CharField(max_length=12)
+    client_ID_number = models.CharField(max_length=20)
 
     def __str__(self):
         return self.client_name

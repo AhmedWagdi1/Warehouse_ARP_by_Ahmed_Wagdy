4d3643acd1f918a5d10216a13dbdc26d7e79aa02
lito <ahmed.w.amin@gmail.com>
2018-11-03 23:57:10 +0200

    Setting database

--- gentelella/settings.py
+++ gentelella/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'gentelella.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASES = {
+#DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),

352aee6b09534395a29dc0a47ba03f307df1a970
lito <ahmed.w.amin@gmail.com>
2018-11-08 14:06:45 +0200

    adde auto flush script

---
+++ app/management/commands/clear_models.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from app import models
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        models.Company.objects.all().delete()
+        models.Farm.objects.all().delete()
+        models.Job.objects.all().delete()
+        models.Worker.objects.all().delete()
+        models.Supplier.objects.all().delete()
+        models.Client.objects.all().delete()
+        models.Product.objects.all().delete()
+        models.Warehouse.objects.all().delete()
+        models.Balance.objects.all().delete()
+        models.Type.objects.all().delete()
+        models.Category.objects.all().delete()
+        models.Daily.objects.all().delete()
+        models.BuyInvoice.objects.all().delete()
+        models.SellInvoice.objects.all().delete()
+        models.Talabat.objects.all().delete()
